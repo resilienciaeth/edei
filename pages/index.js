6bd1d1f9c2d23032c6b68e22b8947326f150f86d
resilienciaeth <dani@nakedmeta.io>
2022-10-01 18:21:01 -0400

    changes-space

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ const newlanding = () => (
         <Image src={images.logoedei} width={600} height={250} />
       </div>
       {/* banner */}
-      <div className="px-6 nm:px-[10rem] mt-10 nm:mt-10">
-        <div className="bg-edei-11 rounded-3xl nm:rounded-[3rem] border-4 nm:border-8 px-4 nm:px-10 ">
-          <h1 className="uppercase text-black text-center nm:text-xl mt-4 font-bold">EDEI is a purposed nft culture of disruptors, entrepreneurs and investors around 3333 edeities ip.</h1>
+      <div className="px-6 nm:px-[10rem] mt-5 nm:mt-10">
+        <div className="bg-edei-11 rounded-3xl nm:rounded-[3rem] border-4 nm:border-8 px-2 nm:px-10 ">
+          <h1 className="uppercase text-black text-sm text-center nm:text-xl mt-4 font-bold">EDEI is a purposed nft culture of disruptors, entrepreneurs and investors around 3333 edeities ip.</h1>
           <p className="mt-4 text-black nm:mt-10 text-base text-center mb-4 nm:text-2xl nm:px-12">An Edeity is a drawn character representing one of the 333 most important things in life to live free from mainstream thinking, money and time.</p>
         </div>
       </div>
       {/* buttons */}
       <div className="flex flex-col items-center justify-center">
-        <div className="mt-10 space-x-4 nm:space-x-10">
+        <div className="mt-4 space-x-4 nm:space-x-10">
           <button
             className="bg-white text-black py-3 px-3 nm:px-10 nm:py-4 rounded-3xl border nm:text-xl font-bold hover:text-white hover:border hover:bg-edei-10 hover:border-white"
             onClick={() => window.open('https://youtu.be/LgbR_2yiqtQ')}
@@ -41,7 +41,7 @@ const newlanding = () => (
       </div>
       {/* WEBUILDING */}
       <div>
-        <h1 className="text-center text-white mt-10 text-xl nm:text-4xl font-bold">EXPECT THE UNEXPECTED #WEBUILDING2023</h1>
+        <h1 className="text-center text-white mt-5 text-xl nm:text-4xl font-bold">EXPECT THE UNEXPECTED #WEBUILDING2023</h1>
         <p className="text-edei-11 font-bold text-center text-xs nm:text-sm px-5">ALREADY PRESOLDOUT 5% OUT OF THE 3333 UNIQUE EDEITIES MADE IN FOUNDER COMMUNITY GENESIS PRESALE</p>
       </div>
       {/* Icons */}
